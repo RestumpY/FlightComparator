2c83f3bfa7487a74a5b3a50ab76c95f7e0037207
P-NAGANATHAN <piravin95400@gmail.com>
2024-02-09 21:24:31 +0100

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 from parse import parse
 from concurrent.futures import ThreadPoolExecutor
+import json
 
 # # Demande à l'utilisateur la destination
 # depart = str(input("Entrez le lieu de départ: "))
@@ -14,9 +15,11 @@ dateArrive = dateDepart + datetime.timedelta(weeks=nbreSemaine)
 
 datesDeparts = []
 datesArrives = []
+destinations = []
 
 # Initialiser la liste de dates
 for i in range(10):
+    destinations.append(destination)
     datesDeparts.append(dateDepart + datetime.timedelta(days=i))
     datesArrives.append(dateArrive + datetime.timedelta(days=i))
 
@@ -24,8 +27,16 @@ def parse_date(destination, dateDepart, dateArrive):
     return parse(destination, dateDepart, dateArrive)
 
 with ThreadPoolExecutor() as executor:
-    results = executor.map(parse_date, [destination]*10, datesDeparts, datesArrives)
+    results = executor.map(parse_date, destinations, datesDeparts, datesArrives)
+    
+# Liste pour stocker tous les items
+all_items = []
 
 for result in results:
     # Traitez le résultat de parsing ici
-    print(result)
+    all_items.extend(result)
+
+with open('items.json', 'w', encoding='utf-8') as json_file:
+    json.dump([item.__dict__ for item in all_items], json_file, ensure_ascii=False, indent=4)
+
+print("Les données ont été enregistrées dans le fichier 'items.json'")
--- parse.py
+++ parse.py
@@ -5,57 +5,63 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from item import Item
 import time
+import json
+
 
 def parse(destination, dateDepart, dateArrive):
     driver = webdriver.Chrome()
-    driver.get("https://www.google.com/travel/flights?hl=fr")
+    try:
+        driver.get("https://www.google.com/travel/flights?hl=fr")
+        
+        # Convertir les dates en format de chaîne de caractères
+        dateDepart = dateDepart.strftime("%d/%m")
+        dateArrive = dateArrive.strftime("%d/%m")
+        consent = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[aria-label="Tout accepter"]')))
+        consent.click()
 
-    # Convertir les dates en format de chaîne de caractères
-    dateDepart = dateDepart.strftime("%d/%m")
-    dateArrive = dateArrive.strftime("%d/%m")
-    consent = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[aria-label="Tout accepter"]')))
-    consent.click()
+        arrival = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')))
+        arrival.send_keys(destination)
+        first_result = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.DFGgtd li:first-child')))
+        first_result.click()
+        time.sleep(1)
+        select_date_depart = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[jscontroller="OKD1oe"] > input[aria-label="Départ"]')))
+        select_date_depart.send_keys(dateDepart)
+        select_date_arrivee = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[jscontroller="OKD1oe"] > input[aria-label="Retour"]')))
+        select_date_arrivee.send_keys(dateArrive)
+        select_date_arrivee.send_keys(Keys.ENTER)
+        time.sleep(1)
+        button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-Bz112c-M1Soyc.nCP5yc.AjY5Oe.LQeN7.TUT4y.zlyfOd'))) 
+        button.click()
 
-    arrival = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')))
-    arrival.send_keys(destination)
-    first_result = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.DFGgtd li:first-child')))
-    first_result.click()
-
-    time.sleep(2)
-
-    select_date_depart = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[jscontroller="OKD1oe"] > input[aria-label="Départ"]')))
-    select_date_depart.send_keys(dateDepart)
-
-
-    select_date_arrivee = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[jscontroller="OKD1oe"] > input[aria-label="Retour"]')))
-    select_date_arrivee.send_keys(dateArrive)
-    select_date_arrivee.send_keys(Keys.ENTER)
-
-    time.sleep(2)
-    button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-Bz112c-M1Soyc.nCP5yc.AjY5Oe.LQeN7.TUT4y.zlyfOd'))) 
-    button.click()
-
-    bestPrice = WebDriverWait(driver, 5).until(
+        bestPrice = WebDriverWait(driver, 5).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'ul.Rk10dc')))
 
-    bestPrice = WebDriverWait(bestPrice[0], 5).until(
+        bestPrice = WebDriverWait(bestPrice[0], 5).until(
         EC.presence_of_all_elements_located((By.TAG_NAME, 'li')))
 
-    items = []
+        items = []
 
-    for price in bestPrice:
-        villeDepart = driver.find_element(By.CSS_SELECTOR, 'div.e5F5td.BGeFcf div.cQnuXe.k0gFV > div > div > input').get_attribute('value')
-        villeArrive = driver.find_element(By.CSS_SELECTOR, 'div.e5F5td.vxNK6d div.cQnuXe.k0gFV > div > div > input').get_attribute('value') 
-        dateDepart= driver.find_element(By.CSS_SELECTOR, 'div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.kStSsc.ieVaIb input').get_attribute('value') 
-        dateArrive = driver.find_element(By.CSS_SELECTOR, 'div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.lJODHb.qXDC9e input').get_attribute('value') 
+        for price in bestPrice:
+            villeDepart = driver.find_element(By.CSS_SELECTOR, 'div.e5F5td.BGeFcf div.cQnuXe.k0gFV > div > div > input').get_attribute('value')
+            villeArrive = driver.find_element(By.CSS_SELECTOR, 'div.e5F5td.vxNK6d div.cQnuXe.k0gFV > div > div > input').get_attribute('value') 
+            dateDepart= driver.find_element(By.CSS_SELECTOR, 'div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.kStSsc.ieVaIb input').get_attribute('value') 
+            dateArrive = driver.find_element(By.CSS_SELECTOR, 'div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.lJODHb.qXDC9e input').get_attribute('value') 
 
-        horaires = price.find_elements(By.CSS_SELECTOR, 'span[jscontroller="cNtv4b"] > span')
-        heureAller = horaires[0].text
-        heureArrive = horaires[1].text
-        duree = price.find_element(By.CSS_SELECTOR, 'div.OgQvJf.nKlB3b > div.Ak5kof > div').text
-        compagnie = price.find_element(By.CSS_SELECTOR,'div.OgQvJf.nKlB3b > div.Ir0Voe > div.sSHqwe.tPgKwe.ogfYpf > span:nth-child(1)').text
-        prix = price.find_element(By.CSS_SELECTOR,'div.U3gSDe span').text
-        
-        item = Item(villeDepart=villeDepart, villeArrive=villeArrive, prix=prix, duree=duree, departDate=dateDepart, retourDate=dateArrive, heureDepart=heureAller, heureArrive=heureArrive, compagnie=compagnie)
-        items.append(item)
-    return items
+            horaires = price.find_elements(By.CSS_SELECTOR, 'span[jscontroller="cNtv4b"] > span')
+            heureAller = horaires[0].text
+            heureArrive = horaires[1].text
+            duree = price.find_element(By.CSS_SELECTOR, 'div.OgQvJf.nKlB3b > div.Ak5kof > div').text
+            compagnie = price.find_element(By.CSS_SELECTOR,'div.OgQvJf.nKlB3b > div.Ir0Voe > div.sSHqwe.tPgKwe.ogfYpf > span:nth-child(1)').text
+            prix = price.find_element(By.CSS_SELECTOR, 'div.U3gSDe').text.split('\n')[0]
+            item = Item(villeDepart=villeDepart, villeArrive=villeArrive, prix=prix, duree=duree, departDate=dateDepart, retourDate=dateArrive, heureDepart=heureAller, heureArrive=heureArrive, compagnie=compagnie)
+            items.append(item)
+        return items
+
+    finally:
+        driver.quit()
+
+
+
+
+
+
