33ff928947a698fdd3b38e13e05cb12f06fe7efa
Piravin NAGANATHAN <piravin95400@gmail.com>
2024-02-09 14:28:15 +0100

    first commit

---
+++ date.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import datetime
+
+class DateWeeks:   
+    def date_flight(self):
+        # Demande à l'utilisateur le nombre de semaines à ajouter
+        nombre_semaines = int(input("Entrez le nombre de semaines à ajouter : "))
+
+        # Ajoute le nombre de semaines à la date actuelle
+        date_actuelle = datetime.datetime.now()
+        date_future = date_actuelle + datetime.timedelta(weeks=nombre_semaines)
+
+        print("Date actuelle :", date_actuelle.strftime("%d/%m"))
+        print("Date future :", date_future.strftime("%d/%m"))
+        
+
+
+dateweek= DateWeeks()
+dateweek.date_flight()
